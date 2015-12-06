ee089811c04876edab983685936a0d6005ff41ed
Hank McLaughlin <hmclaughlin@mindgrub.com>
2015-12-06 11:13:38 -0500

    pulled code from shared workspace

---
+++ Lessons/Lesson4 Game PartI/1 - Setup/main.js
@@ -0,0 +1,28 @@
+// Tutorial source: http://blog.lessmilk.com/how-to-make-flappy-bird-in-html5-1/
+// Phaser docs: http://phaser.io/docs/2.4.4/index
+
+// Initialize Phaser, and create a 400x490px game
+var game = new Phaser.Game(400, 490, Phaser.AUTO, 'gameDiv');
+
+// Create our 'main' state that will contain the game
+var mainState = {
+
+    preload: function() { 
+        // This function will be executed at the beginning     
+        // That's where we load the game's assets  
+    },
+
+    create: function() { 
+        // This function is called after the preload function     
+        // Here we set up the game, display sprites, etc.  
+    },
+
+    update: function() {
+        // This function is called 60 times per second    
+        // It contains the game's logic   
+    },
+};
+
+// Add and start the 'main' state to start the game
+game.state.add('main', mainState);  
+game.state.start('main');  

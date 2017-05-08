baffe57df8817c36231877126163cde58e722c3a
Jake Present <jpresent@mindgrub.com>
2017-05-08 16:47:28 -0400

    updated main.js for step 2

--- main.js
+++ main.js
@@ -5,19 +5,48 @@ var game = new Phaser.Game(700, 500, Phaser.AUTO, 'gameDiv');
 var mainState = {
 
     preload: function() {
-        // This function will be executed at the beginning     
-        // That's where we load the game's assets  
+        //Change the background color to something more sky-like
+        game.stage.backgroundColor = "#6495ED"
+
+        //Load the game sprites
+        game.load.image('marioStand', 'assets/marioStand.png');
+        game.load.image('marioRun1', 'assets/marioRun1.png');
+        game.load.image('marioRun2', 'assets/marioRun2.png');
     },
 
     create: function() {
-        // This function is called after the preload function     
-        // Here we set up the game, display sprites, etc.  
+        //Set the physics system
+        game.physics.startSystem(Phaser.Physics.ARCADE);
+
+        //Display Mario on the screen
+        this.mario = this.game.add.sprite(100,300,'marioStand');
+
+        //Add gravity to Mario to make him fall
+        game.physics.arcade.enable(this.mario);
+        this.mario.body.gravity.y = 1776;
+
+        //Call the 'jump' function when the space bar is hit
+        var spaceKey = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+        spaceKey.onDown.add(this.jump, this);
     },
 
     update: function() {
-        // This function is called 60 times per second    
-        // It contains the game's logic   
+        //If Mario goes out of the frame, the game will restart
+
+        if(this.mario.inWorld == false) {
+            this.restartGame();
+        }
     },
+
+    jump: function() {
+        //Give Mario a vertical velocity
+        this.mario.body.velocity.y = -600;
+    },
+
+    restartGame: function() {
+        //Starts the 'main' state, which returns to the game
+        game.state.start('main');
+    }
 };
 
 // Add and start the 'main' state to start the game
